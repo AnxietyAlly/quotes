33047a322b67f78ba2a291ff427817a57e0b0469
DonDuck1 <malj0042@hz.nl>
2024-01-09 11:48:15 +0100

    First initialisation

---
+++ code/controllers/quotesController.js
@@ -0,0 +1,67 @@
+import Database from 'better-sqlite3';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'variables.env' });
+
+const db = new Database(process.env.DB_PATH, { verbose: console.log });
+
+function getToday() {
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  // This arrangement can be altered based on how we want the date's format to appear.
+  let currentDate = `${day}-${month}-${year}`;
+  console.log(currentDate); // "17-6-2022"
+  return currentDate;
+}
+
+const tempResponse = {
+  meta: {
+    date: getToday(),
+  },
+  data: {
+    message: 'this route is not implemented yet',
+  },
+};
+
+export async function getAllQuotes(req, res) {
+  try {
+    const stmnt = db.prepare("SELECT * FROM quotes");
+    const rows = stmnt.all();
+    const jsonToSend = {
+      meta: {
+        name: "Quotes",
+        title: "All quotes",
+        date: getToday(),
+        originalUrl: `${req.originalUrl}`,
+      },
+      data: []
+    }
+    for (let i = 0; i < rows.length; i++) {
+      jsonToSend.data.push(`/quotes/${rows[i].id}`)
+    }
+    res.status(200).json(jsonToSend);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getSingleQuote(req, res) {
+  try {
+    const params = [req.params.id];
+    const stmnt = db.prepare(`SELECT * FROM quotes where id = ?`);
+    const row = stmnt.get(params);
+    const jsonToSend = {
+      meta: {
+        name: "Quote",
+        title: "Specific quote",
+        date: getToday(),
+        originalUrl: `${req.originalUrl}`,
+      },
+      data: row
+    }
+    res.status(200).json(jsonToSend);
+  } catch (err) {
+    console.log(err);
+  }
+}
--- code/routes/index.js
+++ code/routes/index.js
@@ -1,21 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import {
-  getAllDailyCheckupResults,
-  getSingleDailyCheckupResult,
-  getAllDailyCheckupResultsForUser,
-  getAllDailyCheckupResultsBetweenDates,
-  getAllDailyCheckupResultsBetweenDatesForUser,
-  addNewDailyCheckupResult
-} from '../controllers/progressController.js';
+  getAllQuotes,
+  getSingleQuote
+} from '../controllers/quotesController.js';
 const router = express.Router();
 
 // routes
 router.get('/', (req, res, next) => {
-  res.json('Hi, this is the progress microservice');
+  res.json('Hi, this is the quotes microservice');
 });
 
-router.options('/progress', (req, res, next) => {
+router.options('/quotes', (req, res, next) => {
   try {
     //set header before response
     res.header({
@@ -32,12 +28,7 @@ router.options('/progress', (req, res, next) => {
 });
 
 // get a collection of all the accounts, you can also use a query
-router.get('/dailyCheckupResults', cors(), getAllDailyCheckupResults);
-router.get('/dailyCheckupResults/:id', cors(), getSingleDailyCheckupResult);
-router.get('/user/:userId/dailyCheckupResults', cors(), getAllDailyCheckupResultsForUser);
-router.get('/dateRange/:minDate/:maxDate/dailyCheckupResults', cors(), getAllDailyCheckupResultsBetweenDates);
-router.get('/user/:userId/dateRange/:minDate/:maxDate/dailyCheckupResults', cors(), getAllDailyCheckupResultsBetweenDatesForUser);
-router.post('/dailyCheckupResults', cors(), addNewDailyCheckupResult);
-
+router.get('/quotes', cors(), getAllQuotes);
+router.get('/quotes/:id', cors(), getSingleQuote);
 
 export default router;
